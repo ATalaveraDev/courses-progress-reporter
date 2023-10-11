f3823f42cb05da0227637c700fc0507bc1995342
Angel <ata@loriot.io>
2023-10-11 12:20:03 +0200

    Implementation - /courses/:id GET * Read data from firebase in the backend side

--- src/pages/api/courses/[courseId].js
+++ src/pages/api/courses/[courseId].js
@@ -1,3 +1,9 @@
-export default function handler(req, res) {
-  res.status(200).json({ name: 'Course 1' });
+import { FIREBASE_PATH } from '../../../../secrets/keys';
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const response = await (await fetch(`${FIREBASE_PATH}/${req.query.courseId}.json`)).json();
+
+    res.status(200).json({ ...response });
+  }
 }
